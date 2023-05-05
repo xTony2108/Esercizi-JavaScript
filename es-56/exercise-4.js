e113c28945fcf779157c94bf6d5164f176ee5e65
Antonino Alampi <Alampiantonino99@gmail.com>
2023-05-05 22:38:29 +0200

    Completato esercizio

---
+++ es-56/exercise-4.js
@@ -0,0 +1,59 @@
+function uncompletedNotes(arr) {
+  let notDone = [];
+  arr.forEach((el) => {
+    notDone.push(el.todos.filter(el => el.done == false))
+  });
+  return notDone;
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+console.log(uncompletedNotes(notes));
