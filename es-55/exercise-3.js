46f48d7540806eebf9753e32c381e74729404256
Antonino Alampi <Alampiantonino99@gmail.com>
2023-05-05 21:35:23 +0200

    Completato esercizio

---
+++ es-55/exercise-3.js
@@ -0,0 +1,22 @@
+function calculateAverageAge(arr) {
+  let initialValue = 0;
+  const avg = arr.reduce((a, b, i) => a + b.age, initialValue);
+  return Math.round(avg/arr.length);
+}
+
+const persons = [
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
