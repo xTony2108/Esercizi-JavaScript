8140ae4b96c569d2139bbe02e98e83671e65f529
Antonino Alampi <Alampiantonino99@gmail.com>
2023-05-09 14:59:02 +0200

    fixato esercizio

--- es-54/exercise-2.js
+++ es-54/exercise-2.js
@@ -1,21 +1,21 @@
 function nicknameMap(arr) {
-  let newArr = []
-  arr.forEach(el => {newArr.push(el.name + "-" + el.age)});
-  // arr.forEach(el => {newArr[newArr.length] = el.name + "-" + el.age});
-  return newArr;
+  const mapping = arr.map((el) => {
+    return el.name + "-" + el.age;
+  });
+  return mapping;
 }
 
 const persons = [
-  { name: 'Paul', age: 16 },
-  { name: 'George', age: 17 },
-  { name: 'Lucas', age: 21 },
-  { name: 'Marco', age: 32 },
-  { name: 'Peter', age: 18 },
-  { name: 'Carl', age: 13 },
-  { name: 'Simon', age: 24 },
-  { name: 'Mark', age: 15 },
-  { name: 'Sandra', age: 34 },
-  { name: 'Alice', age: 28 }
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
 ];
 
 const nicknames = nicknameMap(persons);
