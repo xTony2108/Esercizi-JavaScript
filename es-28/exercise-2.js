dcea658cc7248c09d57e8894f39a759b352d1cf6
Antonino Alampi <Alampiantonino99@gmail.com>
2023-05-10 12:01:41 +0200

    Fixato esercizio

--- es-28/exercise-2.js
+++ es-28/exercise-2.js
@@ -4,6 +4,8 @@ const order = {};
 //   console.log('City is required');
 // }
 
-if (!order.customer.address.city) {
-  console.log('City is required');
-}
+const chain = order?.customer?.address?.city;
+
+if (!chain) {
+  console.log("City is required");
+} else console.log(chain);
