aefd8a9e7a738b82ccacc08c6627ced33147fe73
Antonino Alampi <Alampiantonino99@gmail.com>
2023-05-21 21:27:28 +0200

    Completato esercizio

--- es-67/exercise-4.js
+++ es-67/exercise-4.js
@@ -1,6 +1,6 @@
-function sum(...spread) {
+function sum([...spread]) {
   return spread.reduce((a, b) => a + b);
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(numbers));
